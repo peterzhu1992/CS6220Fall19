e95af312cd05256ef358d14623fe489b24e1770d
cs6220 <cs6220@projecthost.jzhucs.com>
2019-11-02 17:40:28 -0400

    More Scripts

--- walmartscripts/sentScoreupdate.sh
+++ walmartscripts/sentScoreupdate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 A_COUNTER=0
 
-cat walmartsenti_1.txt | while read line
+cat walmartsenti_5.txt | while read line
 do
 	read idVar sentimentVar <<< "$line"
 	#echo $idVar $sentimentVar
 	#mysql --defaults-extra-file=./mysqlconfigs.txt -N -e "SELECT id, sentiment, sentScore FROM db.Data WHERE (source = 'Walmart' AND id = $idVar AND sentiment = $sentimentVar);"
-	mysql --defaults-extra-file=./mysqlconfigs.txt -N -e "UPDATE db.Data SET sentScore = 1.0 WHERE (source = 'Walmart' AND id = $idVar AND sentiment = $sentimentVar);"
+	mysql --defaults-extra-file=./mysqlconfigs.txt -N -e "UPDATE db.Data SET sentScore = 5.0 WHERE (source = 'Walmart' AND id = $idVar AND sentiment = $sentimentVar);"
 	#mysql --defaults-extra-file=./mysqlconfigs.txt -N -e "SELECT id, sentiment, sentScore FROM db.Data WHERE (source = 'Walmart' AND id = $idVar AND sentiment = $sentimentVar);"
 
 	#exit
